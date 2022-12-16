c2ef8c1cf67262fee1b134acaeed3dc5ad9d8626
KarenTellez5692 <115842960+KarenTellez5692@users.noreply.github.com>
2022-12-16 12:12:01 -0600

    Delete Semana 4/Programas directory

--- Semana 4/Programas/spring_batch/src/main/java/com/javatechie/spring/batch/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javatechie.spring.batch.repository;
-
-import com.javatechie.spring.batch.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository  extends JpaRepository<Cliente,Integer> {
-}
